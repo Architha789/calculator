263e1c7b3ccbe63297fcc897874ef46fb7183782
Architha789 <archithak78@gmail.com>
2023-10-12 21:50:08 +0530

    first commit

---
+++ calcu.js
@@ -0,0 +1,33 @@
+function  my_function(){
+    a=parseInt(document.getElementById("num1").value);
+    b=parseInt(document.getElementById("num2").value);
+
+    add=a+b;
+    document.getElementById("p1").innerHTML=add;
+}
+
+function  my_function1(){
+    a=parseInt(document.getElementById("num1").value);
+    b=parseInt(document.getElementById("num2").value);
+
+    sub=a-b;
+    document.getElementById("p1").innerHTML=sub;
+}
+
+function  my_function2(){
+    a=parseInt(document.getElementById("num1").value);
+    b=parseInt(document.getElementById("num2").value);
+
+    multiply=a*b;
+    document.getElementById("p1").innerHTML=multiply;
+}
+
+function  my_function3(){
+    a=parseInt(document.getElementById("num1").value);
+    b=parseInt(document.getElementById("num2").value);
+
+    divide=a/b;
+    document.getElementById("p1").innerHTML=divide;
+}
+document.getElementById("p1").innerHTML.style=font
+
